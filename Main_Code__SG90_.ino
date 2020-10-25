a2b0fcfaf0689a1fbe53014e2ecd07232eabaaaf
Scott Crawford Jr <s.crawjr@yahoo.com>
2020-10-25 17:53:51 -0500

    Main_Code.ino
    The current build of IFWS sketch. Currently features HC-SR04 integration

--- Main_Code__SG90_.ino
+++ Main_Code__SG90_.ino
@@ -1,33 +1,75 @@
 // Main code for IFWS (Currently only has servo control (Adding HCsr04))
 #include<Servo.h>
 
-// Initializes servo object 
+// Initializes Servo Object 
 Servo servo1;
 Servo servo2;
 
-// Initial 
+// Initializes Ultrasonic Sensors
+const int trigPinR = 2; // Right Sensor
+const int echoPinR = 3; 
+
+const int trigPinL = 12; // Left Sensor
+const int echoPinL = 11;
+
+long timeR, timeL, cmR, cmL;
+
+// Initializes LED Array
+const int Red = 10;
+const int Green = 9;
+
+// Initializes Water Level Sensors
 
 int pos = 0; // Initializes servo position
 
 void setup() {
  servo1.attach(9); // Attaches servo 1 to pin 9 
  servo2.attach(10); // Attaches servo 2 to pin 10
+ 
+ pinMode(Red, OUTPUT); // Test LED OUTPUT
+ pinMode(Green, OUTPUT); 
+ 
+ // Ultrasonic Sensor GPIO Init
+ pinMode(trigPinR, OUTPUT);
+ pinMode(trigPinL, OUTPUT);
+ pinMode(echoPinR, INPUT);
+ pinMode(echoPinL, INPUT);
+ Serial.begin(9600);
 }
 
 void loop() {
- // Sweeps 180 degrees in increments of 1
-  for (pos = 0; pos <= 90; pos += 1) {
-  servo1.write(pos);
-  servo2.write(pos);
-  delay(15);
+// Command that tells ultrasonic sensors to begin echo tracking
+ digitalWrite(trigPinR, LOW);
+ digitalWrite(trigPinL, LOW);
+ delayMicroseconds(5);
+ digitalWrite(trigPinR, HIGH);
+ digitalWrite(trigPinL, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPinR, LOW);
+ digitalWrite(trigPinL, LOW);
+ 
+ // Calculates Echo Time
+ pinMode(echoPinR, INPUT);
+ pinMode(echoPinL, INPUT);
+ timeR = pulseIn(echoPinR, HIGH);
+ timeL = pulseIn(echoPinL, HIGH);
+ 
+ // Converts Time to Distance
+ cmR = (timeR/2) / 29.1;
+ cmL = (timeL/2) / 29.1;
+ 
+ if (cmR <= 5 || cmL <= 5) {
+  digitalWrite(Red, HIGH);
+  delay(50);
+  digitalWrite(Red, LOW);
+  delay(50);
+  digitalWrite(Red, High);
  }
-
- // Sweeps back to 0 degrees
- // for (pos = 90; pos >= 0; pos -= 1) {
- //  servo1.write(pos);
- //  servo2.write(pos);
- //  delay(15); 
- //}
-  
+ else {
+  digitalWrite(Red, HIGH);
+  digitalWrite(Green, HIGH);
+ }
+ 
+ delay(3000);
 }
 
